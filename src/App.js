961fe8b473228a6006a0cab5aa3c2608b43c6611
mhernandez <mhernandez@idisc.com>
2021-05-08 13:25:25 -0500

    titles colors fix

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import './App.sass';
 function App() {
   return (
     <div className="App">
-        <h1 className="title">Bulma</h1>
-        <p className="subtitle">
-            Modern CSS framework based on{' '}
-            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-                Flexbox
-            </a>
-        </p>
-
-        <div className="field">
-            <div className="control">
-                <input className="input" type="text" placeholder="Input" />
+        <section className="section">
+            <div className="container">
+                <h1 className="title">
+                    Hello World
+                </h1>
+                <p className="subtitle">
+                    My first website with <strong>Bulma</strong>!
+                </p>
             </div>
-        </div>
-
-        <div className="field">
-            <p className="control">
-    <span className="select">
-      <select>
-        <option>Select dropdown</option>
-      </select>
-    </span>
-            </p>
-        </div>
-
-        <div className="buttons">
-            <a href="http://google.com" className="button is-primary">Primary</a>
-            <a href="http://google.com" className="button is-link">Link</a>
-        </div>
+        </section>
     </div>
   );
 }
